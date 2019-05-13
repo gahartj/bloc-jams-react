401911be74478dbc7454c0a7b03c8c01a125b9e4
Jake Gahart <gahartj@gmail.com>
2019-05-13 17:47:27 -0500

    assignment 7 audio playback not completed quite yet.

--- src/components/Album.js
+++ src/components/Album.js
@@ -45,29 +45,39 @@ class Album extends Component {
     }
   }
 
+  handlePlayHover() {
+    console.log('it worked!');
+  }
+
   render() {
     return (
       <section className="album">
         <section id="album-info">
-        <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-          <div className="album-details">
-            <h1 id="album-title">{this.state.album.title}</h1>
-            <h2 className="artist">{this.state.album.artist}</h2>
-            <div id="release-info">{this.state.album.releaseInfo}</div>
-          </div>
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+            <div className="album-details">
+              <h1 id="album-title">{this.state.album.title}</h1>
+              <h2 className="artist">{this.state.album.artist}</h2>
+              <div id="release-info">{this.state.album.releaseInfo}</div>
+            </div>
         </section>
-        <table id="song-list">
-         <colgroup>
-           <col id="song-number-column" />
-           <col id="song-title-column" />
-           <col id="song-duration-column" />
-         </colgroup>
-         <tbody>
+
+      <table id="song-list">
+        <colgroup>
+          <col id="song-number-column" />
+          <col id="song-title-column" />
+          <col id="song-duration-column" />
+        </colgroup>
+        <tbody>
           {this.state.album.songs.map( (song, index) =>
-            <tr className="song" key={index}>{index+1} {song.title} {song.duration}</tr>
+            <tr className="song" key={index}>
+              <ion-icon name ="pause"></ion-icon>
+              <ion-icon name="play"></ion-icon>
+              {index+1} {song.title} {song.duration}
+            </tr>
           )}
-         </tbody>
+        </tbody>
        </table>
+
       </section>
     );
   }
